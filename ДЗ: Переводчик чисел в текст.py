39dc413862a1d209cccdaf1248d3f212a65d2440
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-01 00:02:41 +0300

    Update and rename main.py to ДЗ: Переводчик чисел в текст.py

---
+++ ДЗ: Переводчик чисел в текст.py
@@ -0,0 +1,10 @@
+words = ["One", "Two", "Three", "Four", "Five"]
+
+try:
+    num = int(input("Введите число от 1 до 5: "))
+    if 1 <= num <= 5:
+        print("Английское слово:", words[num - 1])
+    else:
+        print("Ошибка: введите число от 1 до 5.")
+except ValueError:
+    print("Ошибка: введите целое число.")
