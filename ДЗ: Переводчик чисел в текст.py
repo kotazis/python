203189519880db6e483307e8764b62372ab34f71
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-01 00:49:33 +0300

    Update ДЗ: Переводчик чисел в текст.py

--- ДЗ: Переводчик чисел в текст.py
+++ ДЗ: Переводчик чисел в текст.py
@@ -3,7 +3,7 @@ words = ["One", "Two", "Three", "Four", "Five"]
 try:
     num = int(input("Введите число от 1 до 5: "))
     if 1 <= num <= 5:
-        print("Английское слово:", words[num - 1])
+        print("Соответствующее слово:", words[num - 1])
     else:
         print("Ошибка: введите число от 1 до 5.")
 except ValueError:
