99a7561dc8b89f2d96a8e6e9a5dcf0c4e1b7487d
kotazis <95241252+kotazis@users.noreply.github.com>
2025-02-28 19:01:41 +0300

    ДЗ: Логические выражения и условия.py

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-print("Выберите операцию:", "\n1. Сложение", "\n2. Вычитание", "\n3. Умножение", "\n4. Деление")
+age = int(input("Введите возраст "))
+nationality = input("Назоваите страну вашего гражданства ")
+disqualified = input("Являетесь ли вы дисквалифицированным? да/нет ")
 
-try:
-    operation = input("Выберите операцию (1/2/3/4): ")
-    if operation in ['1', '2', '3', '4']:
+if  disqualified == "да":
+    disqualified = True
+else:
+    disqualified = False
 
-        num1 = float(input("Введите первое число: "))
-        num2 = float(input("Введите второе число: "))
+if age >= 18 and nationality == "Россия" and disqualified == False:
+    print("Вы можете голосовать на выборах")
+else:
+    print("Вы не можете голосовать на выборах")
 
-        if   operation == '1':
-             print(f"Результат: {num1} + {num2} = {num1 + num2}")
-        elif operation == '2':
-              print(f"Результат: {num1} - {num2} = {num1 - num2}")
-        elif operation == '3':
-             print(f"Результат: {num1} * {num2} = {num1 * num2}")
-        elif operation == '4':
-              print(f"Результат: {num1} / {num2} = {num1 / num2}")
-
-    else: print("Неверный выбор операции.")
-
-except ValueError:
-    print("Ошибка: Введено не число")
-except ZeroDivisionError:
-    print("Деление на ноль невозможно")
