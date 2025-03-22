e198c2498ea001e350efe7a86315f41bdd69a3ec
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-22 19:51:45 +0300

    Update negative_test.py

--- negative_test.py
+++ negative_test.py
@@ -1,4 +1,5 @@
-# Импортируем необходимые модули
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -7,11 +8,8 @@ from webdriver_manager.chrome import ChromeDriverManager
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 options.add_argument("--headless")
-
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-
 base_url = 'https://www.saucedemo.com/'
-url = 'https://www.saucedemo.com/inventory.html'
 
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
@@ -25,34 +23,7 @@ pass_input.send_keys("secret_sauce")
 button_login = driver.find_element(By.ID, "login-button")
 button_login.click()
 
-#ger_url = driver.current_url
-#assert url == ger_url, "Url не верен"
-#print("Url верен")
-
-# поиск элемента с сообщением об ошибке авторизации
-warning_element = driver.find_element(By.XPATH, "//h3[@data-test='error']")
-# присваивание переменной текст сообщения ошибки
-warning_text = warning_element.text
-
-list_of_warnings = (
-    'Epic sadface: Username is required',
-    'Epic sadface: Password is required',
-    'Epic sadface: Username and password do not match any user in this service'
-)
-
-# проверка наличия ошибки
-if warning_text in list_of_warnings:
-    print("Ошибка авторизации")
-else:
-    print("Авторизация корректна")
-
-# поиск кнопки закрытия сообщения об ошибке
-error_button = driver.find_element(By.XPATH, "//button[@class='error-button']")
-# нажатия на кнопку закрытия сообщения об ошибке
-error_button.click()
-print("click error button")
-
-#text_product = driver.find_element(By.XPATH, "//span[@class='title']")
-#print(text_product.text)
-#assert text_product.text == "Products", "Заголовок не верен"
-#print('Заголовок корректен')
+# функция для задержки кода (3 сек)
+time.sleep(3)
+# функция для обновления страницы браузера
+driver.refresh()
