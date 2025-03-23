8fa8b84f807a7bc4589d227ff858d8e1c89652a1
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-23 15:32:34 +0300

    Update and rename refresh_test.py to imitation_test.py

--- imitation_test.py
+++ imitation_test.py
@@ -1,5 +1,5 @@
 import time
-
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -7,23 +7,35 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+
 base_url = 'https://www.saucedemo.com/'
 
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
-user_name.send_keys("standard_use")
+user_name.send_keys("standard_user")
+
+time.sleep(1)
+
+user_name.send_keys(Keys.CONTROL + "a")
+
+time.sleep(1)
+
+user_name.send_keys(Keys.BACKSPACE)
 
 pass_input = driver.find_element(By.XPATH, "//input[@id='password']")
 pass_input.send_keys("secret_sauce")
 
+time.sleep(1)
+
+pass_input.send_keys(Keys.CONTROL + "a")
+
+time.sleep(1)
+
+pass_input.send_keys(Keys.BACKSPACE)
+
 button_login = driver.find_element(By.ID, "login-button")
 button_login.click()
 
-# функция для задержки кода (3 сек)
-time.sleep(3)
-# функция для обновления страницы браузера
-driver.refresh()
