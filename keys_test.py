6afd2dfac69116356d99d7f8a82084bf1e9802b3
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-23 22:45:37 +0300

    Update keys_test.py

--- keys_test.py
+++ keys_test.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
@@ -16,12 +18,14 @@ driver.set_window_size(1920, 1080)
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 pass_input = driver.find_element(By.XPATH, "//input[@id='password']")
 
-user_name.send_keys(Keys.CONTROL + 'a')
-user_name.send_keys(Keys.DELETE)
-pass_input.send_keys(Keys.CONTROL + 'a')
-pass_input.send_keys(Keys.DELETE)
+user_name.send_keys("123")
+pass_input.send_keys("qweasd")
 
+time.sleep(2)
+
+user_name.send_keys(Keys.CONTROL + 'a')
 user_name.send_keys("standard_user")
+pass_input.send_keys(Keys.CONTROL + 'a')
 pass_input.send_keys("secret_sauce")
 
-button_login = driver.find_element(By.ID, "login-button").click()
+driver.find_element(By.ID, "login-button").click()
