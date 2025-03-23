236f02ca24eb2279ca109de577590058f5aaa783
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-23 19:50:24 +0300

    Update scroll_test.py

--- dz_test.py
+++ dz_test.py
@@ -1,40 +1,32 @@
-# Импортируем необходимые модули
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-
-# Аргумент для настроек запуска браузера, который не открывает само окно браузера
-options.add_argument("--headless")
-
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
 base_url = 'https://www.saucedemo.com/'
 
-url = 'https://www.saucedemo.com/inventory.html'
-
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
-user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
-user_name.send_keys("standard_user")
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys("standard_user")
+pass_input = driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce")
 
-pass_input = driver.find_element(By.XPATH, "//input[@id='password']")
-pass_input.send_keys("secret_sauce")
+button_login = driver.find_element(By.ID, "login-button").click()
 
-button_login = driver.find_element(By.ID, "login-button")
-button_login.click()
+button_backpack = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+button_bike_light = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bike-light"]').click()
+button_bolt_tshirt = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-bolt-t-shirt"]').click()
+button_fleece_jacket = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-fleece-jacket"]').click()
+button_onesie = driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-onesie"]').click()
+button_red_tshirt = driver.find_element(By.XPATH, '//*[@id="add-to-cart-test.allthethings()-t-shirt-(red)"]').click()
 
-ger_url = driver.current_url
+button_cart = driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
 
-assert url == ger_url, "Url не верен"
-print("Url верен")
-
-text_product = driver.find_element(By.XPATH, "//span[@class='title']")
-print(text_product.text)
-
-assert text_product.text == "Products", "Заголовок не верен"
-print('Заголовок корректен')
+action = ActionChains(driver)
+element = driver.find_element(By.ID, 'item_3_title_link')
+action.move_to_element(element).perform()
