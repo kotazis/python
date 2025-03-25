ae0239ea6f2b8934022cd41ab1286605d78048b6
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-25 15:38:58 +0300

    Update and rename keys_test.py to moves.py

--- moves.py
+++ moves.py
@@ -4,7 +4,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
+
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -15,17 +15,21 @@ base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
-user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
-pass_input = driver.find_element(By.XPATH, "//input[@id='password']")
-
-user_name.send_keys("123")
-pass_input.send_keys("qweasd")
-
-time.sleep(2)
-
-user_name.send_keys(Keys.CONTROL + 'a')
-user_name.send_keys("standard_user")
-pass_input.send_keys(Keys.CONTROL + 'a')
-pass_input.send_keys("secret_sauce")
+driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys("standard_user")
+driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce")
 
 driver.find_element(By.ID, "login-button").click()
+
+driver.find_element(By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]').click()
+
+driver.find_element(By.XPATH, '//*[@id="shopping_cart_container"]/a').click()
+
+time.sleep(1)
+
+driver.back()
+
+time.sleep(1)
+
+driver.forward()
+
+
