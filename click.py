6901a5fb761fd2bfce3327d46a47bb1fe0c28a46
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-26 15:06:47 +0300

    Update and rename radio.py to click.py

--- click.py
+++ click.py
@@ -2,21 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver import ActionChains
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
-base_url = 'https://demoqa.com/radio-button'
+base_url = 'https://demoqa.com/buttons'
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
-radio_button = driver.find_element(By.XPATH, '(//label[@class="custom-control-label"])[2]')
-radio_button.click()
+action = ActionChains(driver)
 
-radio_input = driver.find_element(By.XPATH, '//*[@id="impressiveRadio"]')
+doubleclick_button = driver.find_element(By.XPATH, '//*[@id="doubleClickBtn"]')
+action.double_click(doubleclick_button).perform()
 
-if radio_input.is_selected():
-    print("Radio button is active")
-else:
-    print("Radio button is NOT active")
+rightclick_button = driver.find_element(By.XPATH, '//*[@id="rightClickBtn"]')
+action.context_click(rightclick_button).perform()
+
