335596b0314ed595b5fa6e620c26b5b5e7fd9574
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-26 18:02:01 +0300

    Update and rename click.py to date_picker.py

--- date_picker.py
+++ date_picker.py
@@ -1,22 +1,28 @@
+from datetime import datetime, timedelta
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
+
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
 
-base_url = 'https://demoqa.com/buttons'
+base_url = 'https://demoqa.com/date-picker'
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
-action = ActionChains(driver)
+date_input = driver.find_element(By.XPATH,'//input[@id="datePickerMonthYearInput"]')
+date_input.send_keys(Keys.CONTROL + "a")
+date_input.send_keys(Keys.DELETE)
 
-doubleclick_button = driver.find_element(By.XPATH, '//*[@id="doubleClickBtn"]')
-action.double_click(doubleclick_button).perform()
+ten_days = (datetime.now() + timedelta(days=10)).strftime("%m/%d/%Y")
 
-rightclick_button = driver.find_element(By.XPATH, '//*[@id="rightClickBtn"]')
-action.context_click(rightclick_button).perform()
+date_input.send_keys(ten_days)
+date_input.send_keys(Keys.RETURN)
+
+
 
