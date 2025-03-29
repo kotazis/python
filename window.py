2a28aa8167937c249ac3fad3c09e6dd0987d667d
kotazis <95241252+kotazis@users.noreply.github.com>
2025-03-29 15:49:36 +0300

    Update and rename faker.py to window.py

--- window.py
+++ window.py
@@ -1,3 +1,5 @@
+import time
+
 from faker import Faker
 
 from selenium import webdriver
@@ -8,17 +10,23 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
+options.add_argument("--window-size=1920,1080")
 
 driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
-base_url = 'https://www.saucedemo.com/'
+base_url = 'https://demoqa.com/browser-windows'
 driver.get(base_url)
 driver.set_window_size(1920, 1080)
 
-fake = Faker("en_US")
+new_tab = driver.find_element(By.XPATH, '//button[@id="tabButton"]')
+new_tab.click()
+time.sleep(2)
+driver.switch_to.window(driver.window_handles[0])
 
-name = fake.first_name()
-driver.find_element(By.XPATH, '//*[@id="user-name"]').send_keys(name)
-
+new_window = driver.find_element(By.XPATH, '//*[@id="windowButton"]')
+new_window.click()
+driver.switch_to.window(driver.window_handles[2])
+time.sleep(2)
+driver.switch_to.window(driver.window_handles[0])
 
 
 
