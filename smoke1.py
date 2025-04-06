ed241ce9bcf64554f28d2dfd063a8333c2ccfa0a
kotazis <95241252+kotazis@users.noreply.github.com>
2025-04-06 21:15:32 +0300

    Update smoke1.py

--- smoke1.py
+++ smoke1.py
@@ -7,34 +7,41 @@ from selenium.webdriver.support import expected_conditions as ec
 
 
 class Test():
-    def test_select_product(self):
+    def __init__(self):
         options = webdriver.ChromeOptions()
         options.add_experimental_option("detach", True)
-        driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
+        self.driver = webdriver.Chrome(options=options, service=ChromeService(ChromeDriverManager().install()))
         base_url = 'https://www.saucedemo.com/'
-        driver.get(base_url)
-        driver.set_window_size(1920,1080)
+        self.driver.get(base_url)
+        self.driver.set_window_size(1920,1080)
 
-        user_name = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="user-name"]')))
+
+    def log_in(self):
+        user_name = WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="user-name"]')))
         user_name.send_keys("standard_user")
 
-        password = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="password"]')))
+        password = WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="password"]')))
         password.send_keys("secret_sauce")
 
-        button_login = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.ID, "login-button")))
+        button_login = WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.ID, "login-button")))
         button_login.click()
 
-        button_sause_labs_backpack = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')))
+
+    def add_item(self):
+        button_sause_labs_backpack = WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="add-to-cart-sauce-labs-backpack"]')))
         button_sause_labs_backpack.click()
 
-        button_cart = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="shopping_cart_container"]')))
+        button_cart = WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="shopping_cart_container"]')))
         button_cart.click()
 
-        cart_page = WebDriverWait(driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="header_container"]/div[2]/span')))
-
+    
+    def check_page(self):
+        cart_page = WebDriverWait(self.driver, 30).until(ec.element_to_be_clickable((By.XPATH, '//*[@id="header_container"]/div[2]/span')))
         assert cart_page.text == "Your Cart", "Открыта не та страница"
         print("Страница верна")
 
 
 start_test = Test()
-start_test.test_select_product()
+start_test.log_in()
+start_test.add_item()
+start_test.check_page()
