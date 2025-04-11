0f0a1d7fcbc432cae5d54f350cda45b463e757c5
kotazis <95241252+kotazis@users.noreply.github.com>
2025-04-11 13:26:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,19 @@ import requests
 
 class Test:
     def check_code(self):
-         jokes_list  = requests.get("https://api.chucknorris.io/jokes/categories")
-         print(jokes_list.json(), type(jokes_list))
+        #['animal', 'career', 'celebrity', 'dev', 'explicit', 'fashion', 'food', 'history', 'money', 'movie',
+        # 'music', 'political', 'religion', 'science', 'sport', 'travel']
+        category = input("Введите номер категории: ")
+        jokes_list = requests.get("https://api.chucknorris.io/jokes/categories")
+        categories = jokes_list.json()
+        print(categories)
 
-         categories = jokes_list.json()
+        assert category in  categories, "Такой категории нет"
 
-         for category in categories:
-             joke = requests.get(f"https://api.chucknorris.io/jokes/random?category={category}")
-             joke_text = joke.json().get("value")
-             print(f"Категория: {category} | Шутка: {joke_text}")
+        joke = requests.get(f"https://api.chucknorris.io/jokes/random?category={category}")
+        joke_text = joke.json().get("value")
+        print(f"Категория: {category} | Шутка: {joke_text}")
+
 
 joke1 = Test()
 joke1.check_code()
