92ba5976238fb7b587e41d0b096e9d025d8c13b1
kotazis <95241252+kotazis@users.noreply.github.com>
2025-04-26 15:37:35 +0300

    Update test2.py

--- test2.py
+++ test2.py
@@ -1,13 +1,9 @@
 import pytest
 
-@pytest.fixture
-def set_up():
-   print("Вход в систему выполнен")
+
+def test_sending_mail_1(set_up, some):
+    print("Письмо отправлено")
 
 
-def test_sending_mail_1():
-   print("Письмо отправлено")
-
-
-def test_sending_mail_2():
-   print("Письмо отправлено")
+def test_sending_mail_2(set_up, some):
+    print("Письмо отправлено")
