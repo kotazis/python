b6fd9ecfc45fd43351f6e095cde6d7ad74d9bb72
kotazis <95241252+kotazis@users.noreply.github.com>
2025-04-26 15:37:18 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,15 +1,9 @@
 import pytest
 
-@pytest.fixture
-def set_up():
-    print("Вход в систему выполнен")
-    yield
-    print('Произведен выход из системы')
+
+def test_sending_mail_1(set_up, some):
+   print("Письмо отправлено")
 
 
-def test_sending_mail_1(set_up):
-    print("Письмо отправлено")
-
-
-def test_sending_mail_2(set_up):
-    print("Письмо отправлено")
+def test_sending_mail_2(set_up, some):
+   print("Письмо отправлено")
