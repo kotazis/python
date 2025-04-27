b5f411ec9bef6b7994b982680a82cc12b2c96c82
kotazis <95241252+kotazis@users.noreply.github.com>
2025-04-27 16:32:10 +0300

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,9 +1,30 @@
 import pytest
 
+@pytest.mark.run(order=2)
+def test_sending_mail_1(set_up):
+   print("Письмо 1")
 
-def test_sending_mail_1(set_up, some):
-   print("Письмо отправлено")
+
+@pytest.mark.run(order=3)
+def test_sending_mail_2(set_up):
+   print("Письмо 2")
 
 
-def test_sending_mail_2(set_up, some):
-   print("Письмо отправлено")
+@pytest.mark.run(order=1)
+def test_sending_mail_3(set_up):
+   print("Письмо 3")
+
+
+@pytest.mark.run(order=6)
+def test_sending_mail_4(set_up):
+   print("Письмо 4")
+
+
+@pytest.mark.run(order=4)
+def test_sending_mail_5(set_up,):
+   print("Письмо 5")
+
+
+@pytest.mark.run(order=5)
+def test_sending_mail_6(set_up):
+   print("Письмо 6")
