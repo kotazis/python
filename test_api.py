495868dbcf7ead91ee5f16bb65a5ca18a8d28fa9
kotazis <95241252+kotazis@users.noreply.github.com>
2025-05-01 19:33:53 +0300

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -1,12 +1,13 @@
-from utils.api import Google_maps_api
+from utils.api import GooglMapsApi
 from requests import Response
 
-class Test_create_place():
+
+class TestCreatePlace:
     def test_create_new_place(self):
         print("\nМетод POST")
-        result_post: Response = Google_maps_api.create_new_place()
+        result_post: Response = GooglMapsApi.create_new_place()
         check_post = result_post.json()
         place_id = check_post.get("place_id")
 
         print("Метод GET")
-        result_get: Response =  Google_maps_api.get_new_place(place_id)
+        result_get: Response =  GooglMapsApi.get_new_place(place_id)
