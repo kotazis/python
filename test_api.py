de802254ecab3b0dc2dcf659a727a2048a5a34e0
kotazis <95241252+kotazis@users.noreply.github.com>
2025-05-05 20:41:54 +0300

    Update test_api.py

--- test_api.py
+++ test_api.py
@@ -5,21 +5,24 @@ from requests import Response
 class TestCreatePlace:
     def test_create_new_place(self):
         print("\nМетод POST")
-        result_post: Response = GooglMapsApi.create_new_place()
+        api = GooglMapsApi(base_url="https://rahulshettyacademy.com", key="?key=qaclick123")
+
+        result_post: Response = api.create_new_place()
         check_post = result_post.json()
 
         place_id = check_post.get("place_id")
+
         print("Метод GET POST")
-        result_get: Response =  GooglMapsApi.get_new_place(place_id)
+        result_get: Response = api.get_new_place(place_id)
 
         print("Метод PUT")
-        result_put: Response = GooglMapsApi.put_new_place(place_id)
+        result_put: Response = api.put_new_place(place_id)
 
         print("Метод GET PUT")
-        result_get: Response =  GooglMapsApi.get_new_place(place_id)
+        result_get: Response = api.get_new_place(place_id)
 
         print("Метод DELETE")
-        result_delete: Response = GooglMapsApi.delete_new_place(place_id)
+        result_delete: Response = api.delete_new_place(place_id)
 
         print("Метод GET DELETE")
-        result_delete: Response = GooglMapsApi.get_new_place(place_id)
+        result_delete: Response = api.get_new_place(place_id)
