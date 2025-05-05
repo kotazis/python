db8780ac7391b398e960e8b5375416272013d46c
kotazis <95241252+kotazis@users.noreply.github.com>
2025-05-05 20:41:44 +0300

    Update api.py

--- api.py
+++ api.py
@@ -3,25 +3,26 @@ from utils.http_methods import HttpMethods
 
 class GooglMapsApi:
     def __init__(self, base_url, key):
-
-        self.base_url = "https://rahulshettyacademy.com"
-        self.key = "?key=qaclick123"
+        self.base_url = base_url
+        self.key = key
 
     def create_new_place(self):
-        body_json= {
+        body_json = {
             "location": {
                 "lat": -38.383494,
                 "lng": 33.427362
-            }, "accuracy": 50,
+            },
+            "accuracy": 50,
             "name": "Frontline house",
             "phone_number": "(+91) 983 893 3937",
             "address": "29, side layout, cohen 09",
             "types": [
                 "shoe park",
                 "shop"
-             ],
+            ],
             "website": "http://google.com",
-            "language": "French-IN"}
+            "language": "French-IN"
+        }
         post_resource = "/maps/api/place/add/json"
         post_url = self.base_url + post_resource + self.key
         print(post_url)
@@ -31,20 +32,20 @@ class GooglMapsApi:
 
     def get_new_place(self, place_id):
         get_resource = "/maps/api/place/get/json"
-        get_url = self.base_url + get_resource + self.key + "&place_id" + place_id
+        get_url = self.base_url + get_resource + self.key + "&place_id=" + place_id
         print(get_url)
         result_get = HttpMethods.get(get_url)
         print(result_get.text)
-        return  result_get
+        return result_get
 
     def put_new_place(self, place_id):
         put_resource = "/maps/api/place/update/json"
         put_url = self.base_url + put_resource + self.key
         print(put_url)
         put_json = {
-            "place_id" : place_id,
-             "address" : "100 Lenina street, RU",
-            "key" : "qaclick123"
+            "place_id": place_id,
+            "address": "100 Lenina street, RU",
+            "key": "qaclick123"
         }
         result_put = HttpMethods.put(put_url, put_json)
         print(result_put.text)
@@ -59,4 +60,4 @@ class GooglMapsApi:
         }
         result_delete = HttpMethods.delete(delete_url, delete_json)
         print(result_delete.text)
-        return  result_delete
+        return result_delete
