9863775dd975ae0605d8add66272791d8b1205a8
kotazis <95241252+kotazis@users.noreply.github.com>
2025-05-09 15:07:42 +0300

    Update checking.py

--- checking.py
+++ checking.py
@@ -1,11 +1,17 @@
+import json
 from requests import Response
 
 
-class Checking():
+class Checking:
 
     @staticmethod
     def check_status_code(response: Response, status_code):
         assert  status_code == response.status_code, "Ошибка, статус код = " + str(response.status_code)
         print("Успешно, статус код = " + str(response.status_code))
 
+    @staticmethod
+    def check_json_token(response: Response, expected_value):
+        token = json.loads(response.text)
+        assert list(token)  == expected_value, "Ошибка, отсутствие полей"
+        print("Все поля присутствуют")
 
